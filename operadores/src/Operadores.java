596a4f0dbaf3b992c4261a3517e86ea7711ae33a
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-09-06 00:22:24 -0300

    fim operadores unários

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,13 +1,27 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
 
-        numero = - numero;
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+        System.out.println(variavel);
+
+        // int numero = 5;
+
+        // numero = numero + 2;
+
+        // numero++;
+
+        // System.out.println(numero);
+
+        // System.out.println( -- numero);
+
+        // numero = - numero;
         
-        System.out.println(numero);
+        // System.out.println(numero);
 
-        numero = numero * -1;
+        // numero = numero * -1;
 
-        System.out.println(numero);
+        // System.out.println(numero);
     }
 }
