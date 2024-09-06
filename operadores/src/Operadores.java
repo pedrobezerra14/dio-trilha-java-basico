fcc9b621b1c95a6d8365e9bf5ec1c6208c272d94
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-09-06 00:29:51 -0300

    operadores ternários

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,10 +1,18 @@
 public class Operadores {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        int a, b;
 
-        boolean variavel = true;
+        a = 5;
+        b = 5;
 
-        System.out.println(!variavel);
-        System.out.println(variavel);
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+        // System.out.println(variavel);
 
         // int numero = 5;
 
