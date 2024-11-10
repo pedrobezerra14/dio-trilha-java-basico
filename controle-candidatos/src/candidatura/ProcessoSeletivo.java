31d5c71c1c518756421719177c7fca516b7503bd
Pedro Henrique <pedrohrezendeb07@gmail.com>
2024-11-10 14:37:11 -0300

    adicionando ligações para entrar em contato

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,40 @@
 package candidatura;
-
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		imprimirSelecionados();
+		String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+		
+		for(String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
 	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("Contato realizado com sucesso!!!");
+			//elas precisarão sofrer alterações
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+		else
+			System.out.println("Não conseguimos contato com " + candidato + ", número máximo foi de" + tentativasRealizadas + " tentativas");
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
+	}
+	
 	static void imprimirSelecionados() {
 		String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
 		
