2b288ce6b9a865274ff1fb70b91aaafe45284ab6
Pedro Henrique <pedrohrezendeb07@gmail.com>
2024-11-12 07:27:02 -0300

    finalizando encapsulamento

---
+++ pilares-poo/src/aplicativoMensagens/ComputadorPedrinho.java
@@ -0,0 +1,11 @@
+package aplicativoMensagens;
+
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		
+		msn.receberMensagem();
+	}
+}
